7815ad48c5b4ee24bd766c377b699aea897cded9
nancimelchor0-cmyk <nancimelchor0@gmail.com>
2026-05-07 22:38:46 -0600

    Enhance form validation and submission process
    Added validation for age input and improved form submission handling.

--- app.js
+++ app.js
@@ -2,20 +2,19 @@ document.addEventListener('DOMContentLoaded', function() {
   const form = document.getElementById('healthSurveyForm');
   const nivelDolorInput = document.getElementById('nivel-dolor');
   const nivelDolorLabel = document.querySelector('label[for="nivel-dolor"]');
-
   nivelDolorInput.addEventListener('input', function() {
     nivelDolorLabel.textContent = `Nivel de Dolor (${nivelDolorInput.value}/10):`;
   });
 
   form.addEventListener('submit', function(event) {
-    
     const edad = parseInt(document.getElementById('edad').value);
     if (edad < 0 || edad > 120) {
       alert('Por favor, ingrese una edad válida.');
       event.preventDefault(); 
       return;
-    }
+    } 
     event.preventDefault(); 
+
     const formData = new FormData(form);
     const data = {};
     formData.forEach((value, key) => {
